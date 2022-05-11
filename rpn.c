78de22263abc1a4bd27ea80287692d4d38571580
alexm610 <84472549+alexm610@users.noreply.github.com>
2022-05-10 23:54:32 -0700

    second

---
+++ rpn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+ 	int i;
+	char* stack;
+
+	// allocate space for the stack, say size 100 is maximum size for user expression
+	stack = (char*)malloc(100 * sizeof(char));
+
+	
+
+	return 0;
+}
+
+
