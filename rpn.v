9c7fce1fa61bc5a1a5988b8a8e8e9af3532c2807
alexm610 <84472549+alexm610@users.noreply.github.com>
2022-05-19 16:22:46 -0700

    asdf

--- rpn.v
+++ rpn.v
@@ -1,6 +1,6 @@
 `define S_WAIT		4'b0000
-`deeS_WRITE1	4'b0001
-`S_WRITE2	4'b0010
+`define S_WRITE1	4'b0001
+`define S_WRITE2	4'b0010
 
 
 module rpn(KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, CLOCK_50);
@@ -15,17 +15,17 @@ module rpn(KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, CLOCK_50);
 
 	reg_load_enable #(8) STACK_POINTER_REGISTER (CLOCK_50, data_IN, write, data_OUT);
 	multiplexer_2input #(8) SP_Multiplexor (8'b0, (data_OUT + 1'b1), ~KEY[1], data_IN); 
-	RAM STACK (CLOCK_50, data_OUT[7:0], data_OUT[7:0], ~KEY[0], SW[7:0], LEDR[7:0]);
-
+	//RAM STACK (CLOCK_50, data_OUT[7:0], data_OUT[7:0], ~KEY[0], SW[7:0], LEDR[7:0]);
+/*
 	always @(posedge CLOCK_50)
 		if (~KEY[1]) begin // if reset is asserted then keep in wait signl
 			current_state = `S_WAIT;
 		end
 	
-		case (current_state)
-			`S_WAIT: {write} = {1'b0};
-
-		endcase
+		if (KEY[0]) begin
+			current_state = `S_WRITE1			
+		end
+*/
 endmodule
 
 
