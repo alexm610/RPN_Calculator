3676bbc75b9fb28c40ae9e8acbce288d7f35f3fd
Alex Manak <alexmanak@icloud.com>
2022-05-18 23:45:32 -0700

    back to mac

--- rpn.v
+++ rpn.v
@@ -1,3 +1,8 @@
+`define S_WAIT		4'b0000
+`deeS_WRITE1	4'b0001
+`S_WRITE2	4'b0010
+
+
 module rpn(KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, CLOCK_50);
 	input CLOCK_50;
 	input [3:0] KEY;
@@ -6,17 +11,21 @@ module rpn(KEY, SW, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, CLOCK_50);
 	output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
 	wire [7:0] data_IN, data_OUT;
 	reg write;
+	reg [4:0] current_state;
 
-	reg_load_enable #(8) STACK_POINTER_REGISTER (CLOCK_50, data_IN, ~KEY[0], data_OUT);
+	reg_load_enable #(8) STACK_POINTER_REGISTER (CLOCK_50, data_IN, write, data_OUT);
 	multiplexer_2input #(8) SP_Multiplexor (8'b0, (data_OUT + 1'b1), ~KEY[1], data_IN); 
-
-
-/*
-	// instantiate the stack
-		// read and write address lines are read from output of stack pointer. 
 	RAM STACK (CLOCK_50, data_OUT[7:0], data_OUT[7:0], ~KEY[0], SW[7:0], LEDR[7:0]);
-*/
-			
+
+	always @(posedge CLOCK_50)
+		if (~KEY[1]) begin // if reset is asserted then keep in wait signl
+			current_state = `S_WAIT;
+		end
+	
+		case (current_state)
+			`S_WAIT: {write} = {1'b0};
+
+		endcase
 endmodule
 
 
